e7c459eff3a91b0a69eaa4cdb75d575c84f3f4ab
dar10s <dar10s@users.noreply.github.com>
2017-01-01 14:03:27 -0500

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,11 +1,40 @@
+curl
+echo
+ls
+vim
+killall Finder
+diskutil list
+history
+
+npm start
+node
+node version
+
+
+
+
+# -#alias
+
+alias
+open -e ~/.profile
+edit_profile
+reload_profile
+
+# -#GIT
+git 
 
 # -#IONIC
+ionic -v
+cordova -v
+ionic serve
 
 # -#NODE
 
 # -#NPM
 npm install npm@latest -g
 
+# -#SSH
+ssh -x user@location
 
 KEY:
 # -#TOPIC - TO NEW SECTION
